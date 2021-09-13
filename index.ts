afc2cf42d164bc017cd85bd7bfdbfbb900848621
Daniel Boros <dancixx@gmail.com>
2021-09-13 17:45:45 +0200

    typedefs updated

---
+++ index.ts
@@ -0,0 +1,20 @@
+import * as Citation from './src/citations';
+import * as DiscoveryContent from './src/discoveryContent';
+import * as Exercises from './src/exercises';
+import * as Files from './src/files';
+import * as Filter from './src/filter';
+import * as Guides from './src/guides';
+import * as Measurements from './src/measurements';
+import * as Payments from './src/payments';
+import * as Programs from './src/programs';
+import * as RemoteConfig from './src/remoteConfig';
+import * as Statistics from './src/statistics';
+import * as Store from './src/store';
+import * as Teams from './src/teams';
+import * as Users from './src/users';
+import * as WorkoutCategories from './src/workoutCategories';
+import * as Workouts from './src/workouts';
+
+
+
+export {Citation, DiscoveryContent, Exercises, Files, Filter, Guides, Measurements, Payments, Programs, RemoteConfig, Statistics, Store, Teams, Users, WorkoutCategories, Workouts}
