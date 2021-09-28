9e0500722083950f8092899b462f9ab4c834504e
dancixx <dancixx@gmail.com>
2021-09-28 22:20:19 +0200

    ingredient object updated

--- src/recipes.d.ts
+++ src/recipes.d.ts
@@ -1,9 +1,12 @@
 export interface Ingredient {
-    carbohydrate: number;
-    fiber_total_dietary: number;
-    ingredient_code: string;
-    ingredient_name: string;
-    protein: number;
-    total_fat: number;
-    uid: string;
+    key: string;
+    value: {
+        carbohydrate: number;
+        fiber_total_dietary: number;
+        ingredient_code: string;
+        ingredient_name: string;
+        protein: number;
+        total_fat: number;
+        uid: string;
+    };
 }
