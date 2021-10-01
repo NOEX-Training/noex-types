37a10cb3bca09c825d2a59786119642399f6e058
dancixx <dancixx@gmail.com>
2021-10-01 15:19:36 +0200

    Recipe object created

--- src/recipes.ts
+++ src/recipes.ts
@@ -1,3 +1,5 @@
+import { Asset } from './files';
+import { DocumentReference } from '@firebase/firestore-types';
 export interface Ingredient {
     key: string;
     value: {
@@ -6,7 +8,30 @@ export interface Ingredient {
         ingredient_code: string;
         ingredient_name: string;
         protein: number;
+        total_amount?: number;
         total_fat: number;
         uid: string;
     }
+}
+
+export interface Recipe {
+    categories: Array<'Breakfast' | 'Dinner' | 'Lunch'>
+    creationDate: {
+        nanoseconds: number;
+        seconds: number
+    };
+    description: string;
+    mainIngredients?: Ingredient[] | []
+    recipeHeaderUrls: Asset,
+    saladIngredients?: Ingredient[] | []
+    sideIngredients?: Ingredient[] | []
+    title: string
+    trainer: {
+        name: string
+        teamReference: DocumentReference
+        teamUid: string
+        trainerReference: DocumentReference
+        trainerUid: string
+    },
+    uid: string
 }
