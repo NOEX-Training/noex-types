6511a8f4fe124c39e3da4284937ad1c54bb054d9
dancixx <dancixx@gmail.com>
2022-02-08 09:22:11 +0100

    leaderboard object added

--- index.ts
+++ index.ts
@@ -1,24 +1,25 @@
-import * as Challenge from './src/challenge'
-import * as Citation from './src/citations';
-import * as DiscoveryContent from './src/discoveryContent';
-import * as Exercises from './src/exercises';
-import * as Files from './src/files';
-import * as Filter from './src/filter';
-import * as Guides from './src/guides';
-import * as Recipes from './src/recipes'
-import * as Measurements from './src/measurements';
-import * as Notifications from './src/notifications'
-import * as Payments from './src/payments';
-import * as Programs from './src/programs';
-import * as Questions from './src/questions'
-import * as RemoteConfig from './src/remoteConfig';
-import * as Statistics from './src/statistics';
-import * as Store from './src/store';
-import * as Tags from './src/tags';
-import * as Teams from './src/teams';
-import * as Users from './src/users';
-import * as WorkoutCategories from './src/workoutCategories';
-import * as Workouts from './src/workouts';
+import * as Challenge from "./src/challenge";
+import * as Citation from "./src/citations";
+import * as DiscoveryContent from "./src/discoveryContent";
+import * as Exercises from "./src/exercises";
+import * as Files from "./src/files";
+import * as Filter from "./src/filter";
+import * as Guides from "./src/guides";
+import * as LeaderBoards from "./src/leaderBoards";
+import * as Recipes from "./src/recipes";
+import * as Measurements from "./src/measurements";
+import * as Notifications from "./src/notifications";
+import * as Payments from "./src/payments";
+import * as Programs from "./src/programs";
+import * as Questions from "./src/questions";
+import * as RemoteConfig from "./src/remoteConfig";
+import * as Statistics from "./src/statistics";
+import * as Store from "./src/store";
+import * as Tags from "./src/tags";
+import * as Teams from "./src/teams";
+import * as Users from "./src/users";
+import * as WorkoutCategories from "./src/workoutCategories";
+import * as Workouts from "./src/workouts";
 
 export {
     Challenge,
@@ -28,6 +29,7 @@ export {
     Files,
     Filter,
     Guides,
+    LeaderBoards,
     Measurements,
     Notifications,
     Payments,
@@ -41,5 +43,5 @@ export {
     Teams,
     Users,
     WorkoutCategories,
-    Workouts
+    Workouts,
 };
---
+++ src/leaderBoards.ts
@@ -0,0 +1,18 @@
+import { FirebaseFirestoreTypes } from "@react-native-firebase/firestore";
+
+export interface TeamLeaderBoard {
+    [key: string]: {
+        creationDate: FirebaseFirestoreTypes.Timestamp;
+        cardioGoal: number;
+        cardio7: { duration: number; percent: number };
+        cardio30: { duration: number; percent: number };
+        facebookProfilePictureUrl: string;
+        isInChallenge: boolean;
+        isTrainer: boolean;
+        name: string;
+        trainCount: number;
+        uid: string;
+        workoutsLast7Days: number;
+        workoutsLast30Days: number;
+    };
+}
