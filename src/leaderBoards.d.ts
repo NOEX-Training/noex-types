71bf04659f28ccfa8139e082f3f5f045758e7983
dancixx <dancixx@gmail.com>
2022-02-08 09:34:15 +0100

    fixes

--- src/leaderBoards.d.ts
+++ src/leaderBoards.d.ts
@@ -1,23 +1,21 @@
 import { FirebaseFirestoreTypes } from "@react-native-firebase/firestore";
 export interface TeamLeaderBoard {
-    [key: string]: {
-        creationDate: FirebaseFirestoreTypes.Timestamp;
-        cardioGoal: number;
-        cardio7: {
-            duration: number;
-            percent: number;
-        };
-        cardio30: {
-            duration: number;
-            percent: number;
-        };
-        facebookProfilePictureUrl: string;
-        isInChallenge: boolean;
-        isTrainer: boolean;
-        name: string;
-        trainCount: number;
-        uid: string;
-        workoutsLast7Days: number;
-        workoutsLast30Days: number;
+    creationDate: FirebaseFirestoreTypes.Timestamp;
+    cardioGoal: number;
+    cardio7: {
+        duration: number;
+        percent: number;
     };
+    cardio30: {
+        duration: number;
+        percent: number;
+    };
+    facebookProfilePictureUrl: string;
+    isInChallenge: boolean;
+    isTrainer: boolean;
+    name: string;
+    trainCount: number;
+    uid: string;
+    workoutsLast7Days: number;
+    workoutsLast30Days: number;
 }
