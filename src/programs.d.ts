406a51fd780502b2ac59b59c7b7b8dda43e42fac
dancixx <dancixx@gmail.com>
2022-03-21 16:18:41 +0100

    fixes

--- src/programs.d.ts
+++ src/programs.d.ts
@@ -118,7 +118,7 @@ export interface Program {
      * Only for users if they are in challenge mode.
      */
     userChallengeReference?: DocumentReference;
-    workoutsPerWeek?: number;
+    workoutsPerWeek?: string;
 }
 export interface ProgramWithSections extends Program {
     programSections?: Array<ProgramSection>;
