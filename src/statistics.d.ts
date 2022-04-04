e6f459a6e4024ea5663f1a952db12edb67b7ad14
dancixx <dancixx@gmail.com>
2022-04-04 11:44:04 +0200

    app logs added to stats

--- src/statistics.d.ts
+++ src/statistics.d.ts
@@ -6,3 +6,11 @@ export interface ChartData {
     x: number;
     y: number;
 }
+export interface AppOpenLogs {
+    creationDate: {
+        nanoseconds: number;
+        seconds: number;
+    };
+    logs: string[];
+    uid: string;
+}
--- src/statistics.ts
+++ src/statistics.ts
@@ -6,3 +6,12 @@ export interface ChartData {
   x: number;
   y: number;
 }
+
+export interface AppOpenLogs {
+  creationDate: {
+    nanoseconds: number;
+    seconds: number;
+  };
+  logs: string[];
+  uid: string;
+}
